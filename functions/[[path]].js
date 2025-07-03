12c490a646b728aa6d5f37c79bc858bb304120fc
Sinemp <103983721+Sinemp@users.noreply.github.com>
2025-07-03 08:27:06 +0800

    恢复默认token订阅

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -509,9 +509,9 @@ async function handleMisubRequest(context) {
 
     if (profileIdentifier) {
         // [修正] 使用 config 變量
-        if (config.profileToken === 'profiles') {
-            return new Response('For security reasons, you must set a custom Profile Token in the settings before sharing profiles.', { status: 403 });
-        }
+        //if (config.profileToken === 'profiles') {
+        //    return new Response('For security reasons, you must set a custom Profile Token in the settings before sharing profiles.', { status: 403 });
+        //}
         // [修正] 使用 config 變量
         if (!token || token !== config.profileToken) {
             return new Response('Invalid Profile Token', { status: 403 });
